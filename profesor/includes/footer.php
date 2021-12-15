f45a83e7dccc4d09827a32f72e6029ec9c58d111
VictorHugo201 <83247945+VictorHugo201@users.noreply.github.com>
2021-12-15 00:29:50 -0500

    Add files via upload

---
+++ profesor/includes/footer.php
@@ -0,0 +1,19 @@
+   <!-- Essential javascripts for application to work-->
+   <script src="../js/jquery-3.5.1.js"></script>
+   <script src="../js/popper.min.js"></script>
+   <script src="../js/bootstrap.min.js"></script>
+   <script src="../js/main.js"></script>
+   <script src="../js/fontawesome.js"></script>
+   <!-- The javascript plugin to display page loading on top-->
+   <script type="text/javascript" src="../js/plugins/sweetalert.min.js"></script>
+   <!-- Tada Table plugin-->
+   <script type="text/javascript" src="../js/plugins/jquery.dataTables.min.js"> </script>
+   <script type="text/javascript" src="../js/plugins/dataTables.bootstrap.min.js"> </script>
+
+   <script src="../js/plugins/pace.min.js"></script>
+   <script src="js/functions-contenido.js"></script>
+   <script src="js/functions-evaluacion.js"></script>
+
+   </body>
+
+   </html>
